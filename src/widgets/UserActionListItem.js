c0c8cdf52423d184ceb60248560b2f41b63ca71b
Bernard Gaitho <gaithoben@gmail.com>
2021-02-21 09:48:49 +0300

    Add widgets

---
+++ src/widgets/UserActionListItem.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Block } from 'cloudhub-components';
+
+const UserActionListItem = (props) => {
+  const [] = React.useState(0);
+
+  return <Block></Block>;
+};
+
+export default UserActionListItem;
